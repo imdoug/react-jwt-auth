5b75faceb0ecd345170c2bfe0cd66b3889c237e0
Doug Moreira <dougmoreira@Dougs-MBP.lan>
2022-09-25 16:23:55 -0400

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import { Dashboard, Login, SignUp } from './screens'
+import { 
+  Dashboard, 
+  Login, 
+  SignUp
+ } from './screens'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Dashboard />
-      <Login />
-      <SignUp />
+      <div>
+        <Link to="/login">Login</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/signup">Sign Up</Link>
+      </div>
+        <Routes>
+          <Route path='/login' element={<Login/>} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/signup' element={<SignUp/>} />
+        </Routes>
     </div>
+    </Router>
   );
 }
 
