66d750dea8bde0d246b632b4f46dfa34e28f804e
Doug Moreira <dougmoreira@Dougs-MBP.lan>
2022-09-25 23:07:57 -0400

    signup/login/logout - redux

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -1,6 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { login, reset } from '../../app/features/auth/authSlice'
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+  const { email, password} = formData
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state)=>state.auth)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log(formData)
+      const userData = {
+         email, password
+      }
+      dispatch(login(userData))
+  }
+  useEffect(()=>{
+    if(isError){
+      console.log(message)
+    }
+    if(isSuccess || user){
+      navigate('/dashboard')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+  const onChange = (e) =>{
+    setFormData((prevState)=>({
+      ...prevState, [e.target.name]: e.target.value
+    }))
+  }
+
+  if(isLoading){
+    return <p>Loading</p>
+  }
   return (
     <div>
       <div>
@@ -8,14 +47,14 @@ const Login = () => {
       </div>
       <div>
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" required />
+        <input type="email" name="email" id="email" required onChange={onChange}/>
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" required />
+        <input type="password" name="password" id="password" required onChange={onChange} />
       </div>
       <div>
-        <button type="submit" onClick={()=>{}}>Login</button>
+        <button type="submit" onClick={(e)=>{handleSubmit(e)}}>Login</button>
       </div>
     </div>
   )
--- src/screens/SignUp/SignUp.jsx
+++ src/screens/SignUp/SignUp.jsx
@@ -1,6 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { register, reset } from '../../app/features/auth/authSlice'
+
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+    role: ''
+  })
+  const { name, email, password, password2, role} = formData
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state)=>state.auth)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log(formData)
+    if(password !== password2){
+      console.log('password doesnt match')
+    }else{
+      const userData = {
+        name, email, password, role
+      }
+      dispatch(register(userData))
+    }
+    console.log()
+  }
+  useEffect(()=>{
+    if(isError){
+      console.log(message)
+    }
+    if(isSuccess || user){
+      navigate('/dashboard')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+  const onChange = (e) =>{
+    setFormData((prevState)=>({
+      ...prevState, [e.target.name]: e.target.value
+    }))
+  }
+
+  if(isLoading){
+    return <p>Loading</p>
+  }
+
   return (
     <div>
       <div>
@@ -8,30 +57,30 @@ const SignUp = () => {
       </div>
       <div>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" required />
+        <input type="text" name="name" id="name" required  onChange={onChange}/>
       </div>
       <div>
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" required />
+        <input type="email" name="email" id="email" required onChange={onChange}/>
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" required />
+        <input type="password" name="password" id="password" required onChange={onChange} />
       </div>
       <div>
         <label htmlFor="confPassword">Confirm Password</label>
-        <input type="password" name="re-password" id="confPassword" required />
+        <input type="password" name="password2" id="confPassword" required onChange={onChange} />
       </div>
       <div>
         <label htmlFor="role">Role</label>
-        <select name="role" id="role">
+        <select name="role" id="role" onChange={onChange}>
           <option value="x"></option>
           <option value="admin">admin</option>
           <option value="user">user</option>
         </select>
       </div>
       <div>
-        <button type="submit" onClick={()=>{}}>Login</button>
+        <button type="submit" onClick={(e)=>{handleSubmit(e)}}>Sign Up</button>
       </div>
     </div>
   )
